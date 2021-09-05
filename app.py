b5c88bf96373d7a50bb47a1369a698673f00fc99
kamleshkatre <40403371+kamleshkatre@users.noreply.github.com>
2021-09-05 20:18:29 +0530

    added flask apis

--- app.py
+++ app.py
@@ -1,45 +1,46 @@
-from flask import Flask, render_template, request, redirect
-from models import db, SalesModel
 import json
+
+from flask import Flask, request
 import logging
-import src.scripts.util_script as util_script
 
+#user defined modules import
+from database_operations import insert_a_sale_data,get_all_data,create_table,load_training_data
+
+#declaration flask app for discovering all packages
 app = Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db.init_app(app)
 
 
 @app.before_first_request
-def create_table():
-    db.create_all()
-    util_script.load_data()
+def create_tables():
+    create_table()
 
 
-@app.route('/sales/create', methods=['GET', 'POST'])
-def create():
+
+@app.route('/train/sales/create', methods=['GET', 'POST'])
+def add_training_data():
     if request.method == 'GET':
         return {"message":"please hit post request"}
     if request.method == 'POST':
         data=request.get_json(force=True, silent=False, cache=True)
         logging.info(f"post request came with data {data}")
-        sales = SalesModel(data['Item_Weight'],data['Item_Fat_Content'],
-                            data['Item_Visibility'],data['Item_Type'],data['Item_MRP'],
+
+        insert_a_sale_data(data['Item_Identifier'],data['Item_Weight'],data['Item_Fat_Content'],
+                            data['Item_Visibility'],data['Item_Type'],data['Item_MRP'],data['Outlet_Identifier'],
                             data['Outlet_Establishment_Year'],data['Outlet_Size'],
                             data['Outlet_Location_Type'],data['Outlet_Type'],data['Item_Outlet_Sales'])
-        db.session.add(sales)
-        db.session.commit()
 
-        return redirect('/sales/train')
+        return "successfully stored the data"
 
 
-@app.route('/sales/train')
+@app.route('/sales/train',methods=["GET"])
 def RetrieveList():
-    Sales = SalesModel.query.all()
-    return {"Sales":[SalesModel.json(sal) for sal in Sales]}
+    sales = get_all_data()
+    sales = [json.loads(x) for x in sales]
+    return {"Sales":sales}
 
-
+@app.route('/sales/predict',methods= ["GET","POST"])
+def predict():
+    pass
 
 if __name__=='__main__':
     app.run(debug=True,host='localhost', port=5000)
